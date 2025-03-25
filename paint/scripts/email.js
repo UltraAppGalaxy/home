b8007a7394fc913e1c1c8a4c22213b348c690999
Ultra App Galaxy! <primesoftwareprogrammer@gmail.com>
2025-03-25 03:40:34 -0500

    Add files via upload

---
+++ paint/scripts/email.js
@@ -0,0 +1,2 @@
+  function mailto_creativecpp() { s = "gmail.com"; s = "@" + s; s = "gmail" + s; return s; }
+  function mailto_webmaster() { s = "creativecpp"; s = "@" + s; s = "gmail" + s; return s; }
